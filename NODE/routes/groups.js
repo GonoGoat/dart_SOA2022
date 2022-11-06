473b253b820fbbf85058e75ec9dd5edac42e0f9b
Thomas <thomas080601@gmail.com>
2022-11-06 15:08:49 +0100

    V1 Functionality Group
    Create, list members, add user

--- NODE/routes/groups.js
+++ NODE/routes/groups.js
@@ -4,26 +4,66 @@ var pool = require('../db.js');
 
 /* GET groups listing */
 router.get('/', function(req, res, next) {
-  /*pool.query('select g_id, g_name from groups',(err,rows) =>  {
-    if (err) return print("PAS FONCTIONNE");//errors(res,err);
+  pool.query('select g_id, g_name from groups',(err,rows) =>  {
+    if (err) return errors(res,err);
     return res.send(rows.rows);
-  })*/
-  res.send("group listing")
+  })
+});
+
+/* GET selected group*/
+router.get('/detail', async(req, res, next) =>{
+  //Get params in the URL
+  const select = req.query.select;
+
+  pool.query('SELECT g_name FROM groups WhERE g_id=' + select,(err,rows) =>  {
+    if (err) return errors(res,err);
+    return res.send(rows.rows);
+  })
 });
 
 /* GET members listing */
-router.get('/list_member', function(req, res, next) {
-  pool.query('select g_name, u_fname, u_lname from groups left join users on g_u_id = u_id',(err,rows) =>  {
+router.get('/list_member', async(req, res, next) =>{
+  //Get params in the URL
+  const select = req.query.select;
+  
+  pool.query('SELECT u_fname, u_lname FROM groups_users LEFT JOIN users ON u_id = gu_u_id WHERE gu_g_id=' + select,(err,rows) =>  {
     if (err) return errors(res,err);
     return res.send(rows.rows);
   })
 });
 
 /* GET Owner group*/
-router.get('/owner_group', function(req, res, next) {
-  pool.query('select g_name, u_fname, u_lname from groups left join users on g_u_id = u_id',(err,rows) =>  {
+router.get('/owner_group', async(req, res, next) => {
+  //Get params in the URL
+  const select = req.query.select;
+
+  pool.query('SELECT u_fname, u_lname FROM groups LEFT JOIN users ON g_u_id = u_id WHERE g_id=' + select,(err,rows) =>  {
     if (err) return errors(res,err);
     return res.send(rows.rows);
+  })
+});
+
+/* Add member group*/
+router.post('/add_member', async(req, res, next) =>{
+  const query={
+    text : 'INSERT INTO alerts(a_text,a_date,a_u_id) VALUES($1,CURRENT_TIMESTAMP,$2)',
+    values : ['You are invite to join the group : ' + req.query.group,req.query.added],
+  }
+  pool.query(query,(err,rows) =>  {
+    if (err) return errors(res,err);
+    return res.send("User with ID " +req.query.added + " has been succesfully added");
+  })
+});
+
+/* Create a new group */
+router.post('/create_group', async(req, res, next) =>{
+  const query={
+    text : 'INSERT INTO groups(g_name,g_u_id) VALUES($1,$2)',
+    values : [req.query.group,req.query.owner],
+  }
+  pool.query(query,(err,rows) =>  {
+    if (err) return errors(res,err);
+    return res.send("Groups " +req.query.group + " has been succesfully created");
   })
 });
 
--- sm_reader/lib/Groups/Group.dart
+++ sm_reader/lib/Groups/Group.dart
@@ -2,60 +2,112 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:io';
 
+int group = 0;
+var groups = null;
+
+String? enter = null;
+
+String? name_group = null;
+
+int limit = 0;
+
+int curent_user = 0;
+
 Future Start() async {
-  int group;
   do {
-    var url = Uri.http('localhost:3000', '/groups');
-    var response = await http.get(url);
-    print(response.body);
-    //var res = jsonDecode(response.body);
-    print("sorti ici");
-
-    /*res.forEach((obj) => print(
-        "Group ID : " + obj['g_id'] + " Group name : " + obj['g_name'] + "\n"));*/
-
+    print("Enter 'E' to leave the section groups\n");
     print(
-        "Enter '0' to leave the section Groups or Enter the ID of the group you want\n");
-    group = int.parse(stdin.readLineSync()!);
-    if (group != 0) {
-      Selection(group);
-    }
-  } while (group != 0);
-}
-
-void Selection(int group) async {
-  String b;
-  do {
-    final groups = {'select': group};
-    var url = Uri.http('localhost:3000', '/groups/detail', groups);
-    var response = await http.get(url);
-    var res = jsonDecode(response.body);
-
-    print("Enter 'E' to leave the section Details\n");
-    print("To see members of the group enter 'M', for users enter 'U'\n");
-    b = stdin.readLineSync()!;
-    switch (b) {
-      case 'M':
-        List_member();
+        "Do you want to List existings groups (Enter 'L') or create a New One (Enter 'C')\n");
+    enter = stdin.readLineSync()!;
+    switch (enter) {
+      case 'L':
+        await List_Group();
         break;
-      case 'U':
-        //List_user();
+      case 'C':
+        await Create_group();
         break;
       case 'E':
         break;
       default:
         print("Enter unrecognised, please follow the instructions :  \n");
-        sleep(Duration(seconds: 2));
+        sleep(Duration(seconds: 1));
     }
-  } while (b != 'E');
+  } while (enter != 'E');
 }
 
-void List_member() async {
-  var url_2 = Uri.http('localhost:3000', '/groups/list_member');
+Future List_Group() async {
+  do {
+    limit = 0;
+
+    //Take all existing groups
+    var url = Uri.http('localhost:3000', '/groups');
+    var response = await http.get(url);
+    var res = jsonDecode(response.body);
+
+    res.forEach((obj) => print("Group ID : " +
+        (limit = obj['g_id']).toString() +
+        " Group name : " +
+        obj['g_name'] +
+        "\n"));
+
+    print(
+        "Enter '0' to leave the list Groups or Enter the ID of the group you want\n");
+    group = int.parse(stdin.readLineSync()!);
+    if (group != 0 && group <= limit) {
+      assert(res is List);
+      var name = res[group - 1];
+      assert(name is Map);
+      name_group = name['g_name'];
+      groups = {'select': group.toString()};
+      await Selection();
+    } else if (group > limit) {
+      print("Wrong value");
+    }
+  } while (group != 0);
+}
+
+Future Selection() async {
+  do {
+    print("Enter 'E' to leave the section Details of " + name_group!);
+    print("To see members of the group enter 'M', for users enter 'U'\n");
+    enter = stdin.readLineSync()!;
+    switch (enter) {
+      case 'M':
+        await List_member();
+        break;
+      case 'U':
+        await Interact_user();
+        break;
+      case 'E':
+        break;
+      default:
+        print("Enter unrecognised, please follow the instructions :  \n");
+        sleep(Duration(seconds: 1));
+    }
+  } while (enter != 'E');
+}
+
+Future List_member() async {
+  //Take the owner of the group
+  var url_1 = Uri.http('localhost:3000', '/groups/owner_group/', groups);
+  var response_1 = await http.get(url_1);
+  var res_1 = jsonDecode(response_1.body);
+  assert(res_1 is List);
+  var owner = res_1[0];
+  assert(owner is Map);
+  print("The owner of the group " +
+      name_group! +
+      " is " +
+      owner['u_fname'] +
+      " " +
+      owner['u_lname']);
+
+  //Take all the members of a group
+  var url_2 = Uri.http('localhost:3000', '/groups/list_member', groups);
   var response_2 = await http.get(url_2);
   var res_2 = jsonDecode(response_2.body);
 
-  print("Membres du groupe " + res_2['g_name'] + " : \n");
+  print("Members of the groupe " + name_group! + " : \n");
 
   res_2.forEach((member) => print("Firstname : " +
       member['u_fname'] +
@@ -64,43 +116,83 @@ void List_member() async {
       "\n"));
 }
 
-/*void List_user() {
-  int row = users.length;
-  String add;
+Future List_user() async {
+  limit = 0;
 
-  print("Utilisateurs : \n");
+  var url = Uri.http('localhost:3000', '/users');
+  var response = await http.get(url);
+  var res = jsonDecode(response.body);
 
-  for (int i = 0; i < row; i++) {
-    print("ID : " + i.toString() + "  Utilisateur : " + users[i] + "\n");
-  }
+  print("Users : \n");
 
-  print("Voulez-vous ajouter un utilisateurs comme membre ? Oui'O'/Non 'N'\n");
-  add = stdin.readLineSync()!;
-  switch (add) {
-    case 'O':
-      Add_member();
-      break;
-    case 'N':
-      break;
-    default:
-      print("Valeur non reconnu\n");
-      sleep(Duration(seconds: 1));
+  res.forEach((user) => print("User ID : " +
+      (limit = user['u_id']).toString() +
+      " Firstname : " +
+      user['u_fname'] +
+      " Last name : " +
+      user['u_lname'] +
+      "\n"));
+}
+
+Future Interact_user() async {
+  await List_user();
+  do {
+    print("Would you like to add a user to " +
+        name_group! +
+        " Yes(Y) or No(N) ?\n");
+    enter = stdin.readLineSync()!;
+    switch (enter) {
+      case 'Y':
+        await Add_member();
+        break;
+      case 'N':
+        break;
+      default:
+        print("Enter unrecognised\n");
+        sleep(Duration(seconds: 1));
+    }
+  } while (enter != 'N');
+}
+
+Future Add_member() async {
+  print("Enter the ID of the user to add or 0 to leave : \n");
+  int id = int.parse(stdin.readLineSync()!);
+  if (id > 0 && id <= limit) {
+    final user_added = {'added': id.toString(), 'group': name_group};
+    var response = await http
+        .post(Uri.http('localhost:3000', '/groups/add_member/', user_added));
+    print(response.body + "\n");
+    sleep(Duration(seconds: 1));
   }
 }
 
-void Add_member() {
-  print("Entrer l'id de l'utilisateur à ajouter : \n");
-  int id = int.parse(stdin.readLineSync()!);
-  members.add(users[id]);
-  List_member();
-}*/
-
-/*Future Get_members() async {
-  var url = Uri.http('localhost:5432', '/');
-  var response = await http.get(url);
-  return response;
-}*/
-/* MAIN
-print("Bonjour voici un test 3");
-  var members = await Get_members();
-  print(members.body);*/
+Future Create_group() async {
+  await List_user();
+  print("Enter the ID of the curent user\n");
+  curent_user = int.parse(stdin.readLineSync()!);
+  print("Enter a name for the new group\n");
+  String new_group = stdin.readLineSync()!;
+  do {
+    print("Confirm the name of the group : " +
+        new_group +
+        " Accept (A) or Cancel (C)\n");
+    enter = stdin.readLineSync()!;
+    switch (enter) {
+      case 'A':
+        final create_group = {
+          'group': new_group,
+          'owner': curent_user.toString()
+        };
+        var response = await http.post(
+            Uri.http('localhost:3000', '/groups/create_group/', create_group));
+        print(response.body + "\n");
+        sleep(Duration(seconds: 1));
+        break;
+      case 'C':
+        break;
+      default:
+        print("Enter unrecognised\n");
+        sleep(Duration(seconds: 1));
+    }
+  } while (enter != 'A' && enter != 'C');
+}
