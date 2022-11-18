6f9389725f3e3f833e8334963461670faea3e716
Thomas <thomas080601@gmail.com>
2022-11-18 14:54:37 +0100

    Essai 2 Notification
    Erreur DELETE

--- NODE/routes/notifications.js
+++ NODE/routes/notifications.js
@@ -5,11 +5,53 @@ var pool = require('../db.js');
 /* GET Notification listing */
 router.get('/', function(req, res, next) {
     const query={
-        text : 'SELECT a_id, a_text FROM alerts',
+        text : 'SELECT a_date, a_text FROM alerts',
       }
     pool.query(query,(err,rows) =>  {
       if (err) return errors(res,err);
       return res.send(rows.rows);
+    })
+  });
+
+  /*GET INVITATION listing */
+  router.get('/invite', async(req, res, next) => {
+    const query={
+        text : 'SELECT g_name, g_id FROM requests JOIN groups ON g_id = r_g_id WHERE r_u_id =$1',
+        values : [req.query.user]
+      }
+    pool.query(query,(err,rows) =>  {
+      if (err) return errors(res,err);
+      return res.send(rows.rows);
+    })
+  });
+
+/*Accept Invitation -> ADD in groups_users and DEL in requests*/
+  router.post('/invite/accept', async(req, res, next) => {
+    const query_insert={
+        text : 'INSERT INTO groups_users (r_u_id,r_g_id) VALUES($1,$2)',
+        values : [req.query.user,req.query.group]
+      }
+    const query_delete={
+      text : 'DELETE FROM requests WHERE r_u_id = $1 AND r_g_id = $2',
+      values : [req.query.user,req.query.group]
+    }
+    pool.query(query_insert,(err,rows) =>  {
+    })
+    pool.query(query_delete,(err,rows) =>  {
+      //if (err) return errors(res,err);
+      return res.send("Invitation has been succesfully accepted");
+    })
+  });
+
+  /*Decline Invitation */
+  router.post('/invite/delete', async(req, res, next) => {
+    const query_delete={
+      text : 'DELETE FROM requests WHERE r_u_id=$1 AND r_g_id=$2',
+      values : [req.query.user,req.query.group]
+    }
+    pool.query(query_delete,(err,rows) =>  {
+      //if (err) return errors(res,err);
+      return res.send("Invitation has been succesfully deleted");
     })
   });
 
--- sm_reader/lib/Notifications/Notification.dart
+++ sm_reader/lib/Notifications/Notification.dart
@@ -4,8 +4,28 @@ import 'dart:io';
 
 String? enter;
 
+//To make virtual auth
+int curent_user = 0;
+var user;
+
+int limit = 0;
+
+//Take id of group invite
+var invite;
+
+//Param for invitation response
+var answer;
+
 Future Start() async {
   do {
+    //Start virtual auth
+    await List_user();
+    print("Enter the ID of the curent user\n");
+    curent_user = int.parse(stdin.readLineSync()!);
+    //END virtual auth
+
+    user = {'user': curent_user.toString()};
+
     print(
         "Enter 'N' to see Notification, 'I' to see Invitation and 'E' to Exit");
     enter = stdin.readLineSync()!;
@@ -30,23 +50,103 @@ Future List_Notification() async {
   var response = await http.get(url);
   var res = jsonDecode(response.body);
 
-  res.forEach((obj) => print("Notifications ID : " +
-      obj['a_id'].toString() +
+  res.forEach((obj) => print("Notification Date : " +
+      obj['a_date'].toString() +
       "\n Text : \n" +
       obj['a_text'] +
       "\n\n"));
 }
 
 Future List_Invite() async {
-  var url = Uri.http('localhost:3000', '/notifications/Invite');
+  limit = 0;
+
+  var url = Uri.http('localhost:3000', '/notifications/invite/', user);
+  var response = await http.get(url);
+  invite = jsonDecode(response.body);
+
+  invite.forEach((obj) => print("Invitation number : " +
+      (limit += 1).toString() +
+      " FROM : " +
+      obj['g_name'] +
+      "\n"));
+
+  await Response_Invite();
+}
+
+Future Response_Invite() async {
+  int choice;
+  do {
+    print(
+        "Do you want to accept or decline an invitation ?\n Enter is number or enter 0 to leave\n");
+    choice = int.parse(stdin.readLineSync()!);
+    if (choice != 0 && choice <= limit) {
+      print("Enter 'A' to accept, 'D' to decline or 'E' to come back");
+      enter = stdin.readLineSync()!;
+      switch (enter) {
+        case 'A':
+          await Accept_Invite(choice);
+          break;
+        case 'D':
+          await Decline_Invite(choice);
+          break;
+        case 'E':
+          break;
+        default:
+          print("Enter unrecognised\n");
+          sleep(Duration(seconds: 1));
+      }
+    }
+  } while (choice != 0);
+}
+
+Future Accept_Invite(int choice) async {
+  assert(invite is Map);
+  var selected = invite[choice - 1];
+  assert(selected is List);
+
+  answer = {
+    'user': curent_user.toString(),
+    'group': selected['g_id'].toString()
+  };
+
+  var response = await http
+      .post(Uri.http('localhost:3000', '/notifications/accept/', answer));
+  print(response.body + "\n");
+  sleep(Duration(seconds: 1));
+}
+
+Future Decline_Invite(int choice) async {
+  assert(invite is Map);
+  var selected = invite[choice - 1];
+  assert(selected is List);
+
+  answer = {
+    'user': curent_user.toString(),
+    'group': selected['g_id'].toString()
+  };
+
+  var response = await http
+      .post(Uri.http('localhost:3000', '/notifications/delete/', answer));
+  print(response.body + "\n");
+  sleep(Duration(seconds: 1));
+}
+
+//START to delete
+Future List_user() async {
+  limit = 0;
+
+  var url = Uri.http('localhost:3000', '/users');
   var response = await http.get(url);
   var res = jsonDecode(response.body);
 
-  res.forEach((obj) => print("Notifications ID : " +
-      obj['g_id'].toString() +
-      " Text : \n" +
-      obj['g_name'] +
-      "\n\n"));
-}
+  print("Users : \n");
 
-Future Response_Invite() async {}
+  res.forEach((user) => print("User ID : " +
+      (limit = user['u_id']).toString() +
+      " Firstname : " +
+      user['u_fname'] +
+      " Last name : " +
+      user['u_lname'] +
+      "\n"));
+}
+//END to delete
