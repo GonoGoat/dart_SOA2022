e9ff3a00cc94f3c599d6f140a59d1fe8f4009574
Thomas <thomas080601@gmail.com>
2022-11-23 13:54:14 +0100

    Creation folder Admin

--- sm_reader/bin/sm_reader.dart
+++ sm_reader/bin/sm_reader.dart
@@ -4,6 +4,7 @@ import 'package:sm_reader/auth/Auth.dart' as auth;
 import 'package:sm_reader/Groups/Group.dart' as group;
 import 'package:sm_reader/News/New.dart' as news;
 import 'package:sm_reader/Notifications/Notification.dart' as notification;
+import 'package:sm_reader/Admin/Admin.dart' as admin;
 
 void main(List<String> arguments) async {
   String menu;
@@ -32,7 +33,7 @@ void main(List<String> arguments) async {
       }
     } else {
       print(
-          "What part of the app would you like to access ?\n\tL : Logout\n\tG : Group\n\tN : News\n\tO : Notification\nYou can also press 'E' to leave");
+          "What part of the app would you like to access ?\n\tL : Logout\n\tG : Group\n\tN : News\n\tO : Notification\n\tE : To leave\n");
       menu = stdin.readLineSync()!;
       switch (menu) {
         case 'L':
