976720d20c2b3257a39c02014271bf2d8b67c195
GonoGoat <gauth.vrsgn@outlook.be>
2022-12-15 18:36:20 +0100

    Correction merge

--- sm_reader/bin/sm_reader.dart
+++ sm_reader/bin/sm_reader.dart
@@ -36,64 +36,51 @@ void main(List<String> arguments) async {
     }
     else {
       if (user['hasPaid']) {
-        print("What part of the app would you like to access ?\n\tL : Logout\n\tG : Group\n\tN : News\n\tS : SM\nYou can also press 'E' to leave");
+        await notification.Start(user['id']);
+        if (user['isadmin'] == true) {
+          print("You are login as Administrator. \n" +
+            "What part of the app would you like to access ?\n" +
+            "\tA : Admin section\n\tL : Logout\n\tG : Group\n\tN : News\n\tS : SM\n\tO : Notifications\n\tE : To leave"
+          );
+        }
+        else {
+          print(
+            "What part of the app would you like to access ?\n\tL : Logout\n\tG : Group\n\tN : News\n\tS : SM\n\tO : Notifications\n\tE : To leave"
+          );
+        }
         menu = stdin.readLineSync()!;
         switch (menu) {
+          case 'A':
+            if (user['isadmin'] == true) {
+              await admin.Start(user['id']);
+            }
+            else {
+              print("Enter unrecognised\nPress Enter to continue");
+              stdin.readLineSync();
+            }
+            break;
           case 'L':
             user = await auth.logout() ? null : user;
             break;
           case 'G':
-            Group.Start();
+            await group.Start(user['id']);
             break;
           case 'N':
-            New.Start();
+            await news.Start();
+            break;
+          case 'O':
+            await notification.Start(user['id']);
+            break;
+          case 'E':
             break;
           case 'S' :
             await sm.start(user['id']);
             break;
-          case 'E':
-            break;
           default:
-            print("Choix non valide");
-            sleep(Duration(seconds: 1));
-        }
-    } else {
-      await notification.Start(user['id']);
-      if (user['isadmin'] == true) {
-        print("You are login as Administrator. \n" +
-            "What part of the app would you like to access ?\n" +
-            "\tA : Admin section\n\tL : Logout\n\tG : Group\n\tN : News\n\tO : Notifications\n\tE : To leave");
-      } else {
-        print(
-            "What part of the app would you like to access ?\n\tL : Logout\n\tG : Group\n\tN : News\n\tO : Notifications\n\tE : To leave");
-      }
-      menu = stdin.readLineSync()!;
-      switch (menu) {
-        case 'A':
-          if (user['isadmin'] == true) {
-            await admin.Start(user['id']);
-          } else {
             print("Enter unrecognised\nPress Enter to continue");
             stdin.readLineSync();
-          }
-          break;
-        case 'L':
-          user = await auth.logout() ? null : user;
-          break;
-        case 'G':
-          await group.Start(user['id']);
-          break;
-        case 'N':
-          await news.Start();
-          break;
-        case 'O':
-          await notification.Start(user['id']);
-          break;
-        case 'E':
-          break;
-        default:
-          print("Enter unrecognised\nPress Enter to continue");
-          stdin.readLineSync();
+        }
+      }
       else {
         print("It seems that your yearly subscription is not active. What do you want to do ?\n\tL : Logout\n\tP : Pay\n\tE : Leave\n");
         menu = stdin.readLineSync()!;
