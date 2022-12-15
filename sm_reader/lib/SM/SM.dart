ca0d5864ae7b678fc50a47a2608f7031007b31e3
GonoGoat <gauth.vrsgn@outlook.be>
2022-12-15 17:30:42 +0100

    Search SM

--- sm_reader/lib/SM/SM.dart
+++ sm_reader/lib/SM/SM.dart
@@ -11,6 +11,17 @@ Future listSM() async{
   return res.body;
 }
 
+Future getSMByName() async {
+  print('\nPlease type the name of the song you want to search\n');
+  var str = stdin.readLineSync()!;
+  var res = await http.get(Uri.http('localhost:3000','/sm/name'),);
+  var data = jsonDecode(res.body);
+  for (int i = 0;i<data.length;i++) {
+    print("SM ${i+1}\n\tInstrument : ${data[i]['i_name']}\n\tSong : ${data[i]['s_name']}\n");
+  }
+  return res.body;
+}
+
 Future start(int id) async{
   print("What SM would you like to access\n\tS : Search for a SM\n\tG : Groups favourite SM\n\tE : Leave");
   var menu;
@@ -33,21 +44,22 @@ Future start(int id) async{
 }
 
 Future anySM(int id) async{
-  
-  var search;
+  var search,sm;
   do {
     print("\nHow would you like to search a SM ?\n\tN : By SM name\n\tI : By instrument\n\tE : Leave");
     search = stdin.readLineSync()!;
+    switch (search) {
+      case 'N' :
+        sm = await getSMByName();
+        break;
+      case 'I':
+        sm = await getSMByInstrument();
+        break;
+      default:
+        print("Choix non valide");
+        sleep(Duration(seconds: 1));
+    }
   } while(search != 'E');
-
-  sleep(Duration(seconds: 5));
-  var sm = await listSM();
-
-  var index = 0;
-  do {
-    print("\nPlease select one SM with its identification number\n");
-    index = int.parse(stdin.readLineSync()!);
-  } while(index > 0 && index <= sm.length);
 
   var menu;
   print("\nWhat would you like to do with the following song : ${sm[index]['i_name']} (${sm[index]['s_name']})\n\tR : Read\n\tF : Set as group favourite\n\tE : Leave\n");
