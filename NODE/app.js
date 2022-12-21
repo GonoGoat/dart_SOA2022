8dcedf565c53055e96f8720d4c3c01d0792e64e8
GonoGoat <gauth.vrsgn@outlook.be>
2022-12-21 23:38:19 +0100

    Removing useless node packages

--- NODE/app.js
+++ NODE/app.js
@@ -1,6 +1,4 @@
-var createError = require('http-errors');
 var express = require('express');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var usersRouter = require('./routes/users');
@@ -17,7 +15,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/users', usersRouter);
 app.use('/groups',groupsRouter);
@@ -28,20 +25,9 @@ app.use('/sm', smRouter);
 app.use('/bank',bankRouter);
 app.use("/instruments",instrumentsRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(404).send(false);
 });
 
 module.exports = app;
